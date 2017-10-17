c613cae553b5df01b8b317f69b44a2e7be62f316
chimchim <nicke_lin_91@hotmail.com>
2017-10-17 16:37:09 +0200

    Build console ok

--- Assets/Game/Misc/BuildConsole.cs
+++ Assets/Game/Misc/BuildConsole.cs
@@ -8,14 +8,62 @@ public class BuildConsole : MonoBehaviour
 	public Text LogTemplate;
 	public Text ExceptionTemplate;
 	public int MaxLogs = 15;
-	[HideInInspector]
-	public List<Text> Texts = new List<Text>();
-	[HideInInspector]
-	public int LogIndex;
+
+	private List<Text> _texts = new List<Text>();
+
+	public int _logIndex;
+	int b = 0;
+	private bool _isScrolled;
 	private void Update()
 	{
 		var wheel = Input.GetAxis("Mouse ScrollWheel");
-		Debug.Log("wheel " + wheel);
+		if (wheel != 0 && _texts.Count > MaxLogs)
+		{
+			_isScrolled = true;
+			int wheelValue = (int)(wheel * 10);
+
+			if (wheelValue > 0 && _logIndex > MaxLogs -1)
+			{
+				for (int i = _logIndex - 1; i >= _logIndex - wheelValue; i--)
+				{	if (i < _texts.Count)
+						break;
+					_texts[i].gameObject.SetActive(false);
+				}
+			}
+
+			if (wheelValue < 0)
+			{
+				for (int i = _logIndex - MaxLogs; i < (_logIndex - MaxLogs) - wheelValue; i++)
+				{
+					if (i+MaxLogs >= _texts.Count)
+					{
+						_isScrolled = false;
+                        break;
+					}
+					_texts[i].gameObject.SetActive(false);
+				}
+			}
+			_logIndex -= wheelValue;
+		}
+		_logIndex = Mathf.Clamp(_logIndex, MaxLogs, _texts.Count - 1);
+		if(!_isScrolled)
+			_logIndex = _texts.Count;
+		if (_isScrolled && _logIndex > MaxLogs)
+		{
+			for (int i = _logIndex -1; i > (_logIndex - MaxLogs); i--)
+			{
+				_texts[i].gameObject.SetActive(true);
+            }
+		}
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			for (int i = 0; i < _texts.Count; i++)
+			{
+				GameLobby.Destroy(_texts[i].gameObject);
+			}
+			_texts.Clear();
+			_isScrolled = false;
+		}
 	}
 	void OnEnable()
 	{
@@ -42,11 +90,17 @@ public class BuildConsole : MonoBehaviour
 		
 		text.transform.parent = transform;
 		text.text = logString;
-		Texts.Add(text);
-		text.gameObject.SetActive(true);
-		if (Texts.Count > MaxLogs)
+		_texts.Add(text);
+
+		if (!_isScrolled)
 		{
-			Texts[Texts.Count - (MaxLogs - 1)].gameObject.SetActive(false);
+			_logIndex = _texts.Count;
+		}
+		text.gameObject.SetActive(!_isScrolled);
+		
+		if (_texts.Count > MaxLogs)
+		{
+			_texts[_texts.Count - (MaxLogs + 1)].gameObject.SetActive(false);
 		}
 	}
 }
